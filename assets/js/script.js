8f5e2b300c3202a3ffd4359a04d9e7df5681ffea
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2024-09-23 16:14:18 -0500

    update functions and functionalitys

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,15 +17,33 @@ let formater = new Intl.NumberFormat("es-CO", {
   maximumFractionDigits: 0,
 });
 
-const add = (event) => {
-  event.preventDefault();
+//creamos una funciona que muestra una alerta personalizable
+const showAlert = (text, color) => {
+  if(!color) document.querySelector(".redAlert").style.backgroundColor = 'red';
+  else document.querySelector(".redAlert").style.backgroundColor = color;
 
+  document.querySelector(".redAlert").textContent = text;
+
+  document.querySelector(".redAlert").style.display = "flex";
+};
+
+//Funcion para ocultar la alerta mostrada en tiempo personalizable
+const hiddeAlert = (time) => {
+  if(!time) time = 2000;
+  setTimeout(() => {
+    document.querySelector(".redAlert").style.display = "none";
+  }, time)
+};
+
+//Funcion para agregar una nuevo gasto
+const add = () => {
   let expenseName = document.querySelector("#nombreGasto").value;
   let expenseValue = document.querySelector("#valorGasto").value;
   let id = crypto.randomUUID();
 
   if (!expenseName) {
-    alert("Por favor, complete todos los campos");
+    showAlert('El gasto debe tener al menos un nombre');
+    hiddeAlert();
     return;
   } else {
     const newExpense = new Expense(expenseName, Number(expenseValue), id);
@@ -35,12 +53,11 @@ const add = (event) => {
     document.querySelector("#nombreGasto").value = "";
     document.querySelector("#valorGasto").value = "";
 
-    console.log(expenses);
-
     showExpenses();
   }
 };
 
+//Funcion para mostrar la lista de gastos, cada vez que se llama se actualiza toda la vista
 const showExpenses = () => {
   const expenseList = document.querySelector("#listaDeGastos");
   //Variable para almacenar la lista de gastos
@@ -78,36 +95,80 @@ const showExpenses = () => {
   )}`;
 };
 
+//Funcion para eliminar un gasto
 const remove = (id) => {
   //obtener el indice el elemnto a eliminar
-  let elementToRemove = expenses.findIndex((expense) => expense.id == id);
+  let indexRemove = expenses.findIndex((expense) => expense.id == id);
 
   //con el indice removemos el elemento de la lista
-  expenses.splice(elementToRemove, 1);
+  expenses.splice(indexRemove, 1);
 
   //actualizamos la tabla
   showExpenses();
 };
 
 const update = (id) => {
+
+  showAlert('Modifique el nombre y/o el valor del gasto', 'blue')
+
+  //obtenemos todos los elementos del DOM que vamos a necesitar
+  const nombreGasto = document.querySelector("#nombreGasto");
+  const valorGasto = document.querySelector("#valorGasto");
+  const btnAdd = document.querySelector(".btnAdd");
+  const btnSaveChanges = document.querySelector(".btnSaveChanges");
+
   //Obtenemos el indice del elemento a actualizar
-  let elementToRemove = expenses.findIndex((expense) => expense.id == id);
+  let indexUpdate = expenses.findIndex((expense) => expense.id == id);
 
-  //Obtenemos los nuevos valores
-  let newName = prompt("Ingrese el nuevo nombre del gasto");
-  let newValue = prompt("Ingrese el nuevo valor del gasto");
+  //Desabilitamos los botones de modificar y eliminar de los demas elementos para evitar errores
+  document.querySelectorAll(".btnUpdate").forEach((btn) => {
+    btn.disabled = true;
+    btn.classList.add("not-allowed");
+  });
+  document.querySelectorAll(".btnRemove").forEach((btn) => {
+    btn.disabled = true;
+    btn.classList.add("not-allowed");
+  });
 
-  console.log(newName);
-  console.log(newValue);
+  //Obtenemos los valores y los mostramos en los inputs
+  nombreGasto.value = expenses[indexUpdate].name;
+  valorGasto.value = expenses[indexUpdate].value;
 
-  //Verificamos que existan valores a modificar y modificamos
-  if (newName != "" && newName != null) {
-    expenses[elementToRemove].name = newName;
-  }
-  if (newValue != "" && newValue != null) {
-    expenses[elementToRemove].value = Number(newValue);
-  }
+  //Cambiamos el boton
+  btnAdd.style.display = "none";
+  btnSaveChanges.style.display = "block";
 
-  //Actualizamos la interfaz
-  showExpenses();
+  btnSaveChanges.onclick = () => { //con esta accion en lugar de usar addEventListener aseguramos que se ejecute una sola vez
+                                    //en cada llamada a la funcion, asi evitamos posibles errores al momento de guardar cambios.
+
+    const currentIndex = indexUpdate;
+    //Nuevos valores
+    let newName = nombreGasto.value;
+    let newValue = valorGasto.value;
+
+    // Verificamos que existan valores a modificar y modificamos
+    if (newName != "" && newName != null) {
+      expenses[currentIndex].name = newName;
+    }
+    if (newValue != "" && newValue != null) {
+      expenses[currentIndex].value = Number(newValue);
+    }
+
+    //Mostramos mensaje de exito
+    showAlert('Gasto modificado con éxito!', 'green');
+
+    //Actualizamos la interfaz
+    showExpenses();
+
+    //resetar inputs
+    nombreGasto.value = "";
+    valorGasto.value = "";
+
+    //cambiar el boton otra vez
+    btnAdd.style.display = "block";
+    btnSaveChanges.style.display = "none";
+
+    //Quitamos el mensaje
+    hiddeAlert()
+  };
 };
